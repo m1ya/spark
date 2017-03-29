b0c98889cccf2b654ea1d2f44fe232773135b9dd
Rinaya <ayari@M-SatoAyari.local>
2017-03-30 08:29:51 +0900

    Spark

---
+++ Assets/SlotScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotScript : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.stopFlag)
+			return;
+		transform.position += Vector3.down * speed;
+		if (transform.position.y <= -17f) {
+			transform.position = new Vector3(transform.position.x, 16.9f, transform.position.z);
+		}
+	}
+}
